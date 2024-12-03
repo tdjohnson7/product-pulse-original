38b72f28103d9077413448b8d7b84f73136e5bfa
tdjohnson7 <tdjohnson7@msn.com>
2024-12-03 14:46:00 -0600

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
 const passport = require("passport");
 const validator = require("validator");
 const Customer = require("../models/Customer");
-const Company = require("../models/Company")
-const cloudinary = require("../middleware/cloudinary")
+const Company = require("../models/Company");
+const cloudinary = require("../middleware/cloudinary");
 
 exports.getCustomerLogin = (req, res) => {
   if (req.user) {
@@ -50,7 +50,7 @@ exports.postCustomerLogin = (req, res, next) => {
         return next(err);
       }
       req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/customerProfile");
+      res.redirect(req.session.returnTo || "/feed");
     });
   })(req, res, next);
 };
@@ -90,8 +90,8 @@ exports.postCompanyLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
-  })
+    console.log("User has logged out.");
+  });
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
@@ -170,76 +170,73 @@ exports.postCustomerSignup = (req, res, next) => {
   );
 };
 
+exports.postCompanySignup = async (req, res, next) => {
+  try {
+    console.log("req.body.email", req.body);
+    const validationErrors = [];
 
-  exports.postCompanySignup = async (req, res, next) => {
-    try {
-      console.log("req.body.email", req.body);
-      const validationErrors = [];
-  
-      if (!validator.isEmail(req.body.email))
-        validationErrors.push({ msg: "Please enter a valid email address." });
-      if (!validator.isLength(req.body.password, { min: 8 }))
-        validationErrors.push({ msg: "Password must be at least 8 characters long" });
-      if (req.body.password !== req.body.confirmPassword)
-        validationErrors.push({ msg: "Passwords do not match" });
-  
-      if (validationErrors.length) {
-        req.flash("errors", validationErrors);
-        return res.redirect("../companySignup");
-      }
-  
-      req.body.email = validator.normalizeEmail(req.body.email, {
-        gmail_remove_dots: false,
+    if (!validator.isEmail(req.body.email))
+      validationErrors.push({ msg: "Please enter a valid email address." });
+    if (!validator.isLength(req.body.password, { min: 8 }))
+      validationErrors.push({
+        msg: "Password must be at least 8 characters long",
       });
-  
-      // Await the Cloudinary upload
-      let result
-      if(req.file){
-        try {
-          result = await cloudinary.uploader.upload(req.file.path);
-        }
-         catch(error){
-            console.log("Error uploading image to Cloudinary:", error)
-          }
-        }
-        else{
-            console.log('No file to upload')
-          }
-        
-       const newCompany = new Company({
-        companyName: req.body.companyName,
-        email: req.body.email,
-        password: req.body.password,
-        image: result ? result.secure_url : undefined,
-        cloudinaryId: result ? result.public_id : undefined,
-        companyDescription: req.body.companyDescription
-      });
-  
-      // Await the database query
-      const existingCompany = await Company.findOne({
-        $or: [{ email: req.body.email }, { companyName: req.body.companyName }]
-      });
-  
-      if (existingCompany) {
-        req.flash("errors", {
-          msg: "Account with that email address or company name already exists.",
-        });
-        return res.redirect("../companySignup");
-      }
-  
-      // Await the save operation
-      await newCompany.save();
-  
-      req.logIn(newCompany, (err) => {
-        if (err) {
-          return next(err);
-        }
-        res.redirect("/companyProfile");
-        console.log('company is logged in')
-      });
-  
-    } catch (err) {
-      console.log(err);
-      
+    if (req.body.password !== req.body.confirmPassword)
+      validationErrors.push({ msg: "Passwords do not match" });
+
+    if (validationErrors.length) {
+      req.flash("errors", validationErrors);
+      return res.redirect("../companySignup");
     }
-  };
+
+    req.body.email = validator.normalizeEmail(req.body.email, {
+      gmail_remove_dots: false,
+    });
+
+    // Await the Cloudinary upload
+    let result;
+    if (req.file) {
+      try {
+        result = await cloudinary.uploader.upload(req.file.path);
+      } catch (error) {
+        console.log("Error uploading image to Cloudinary:", error);
+      }
+    } else {
+      console.log("No file to upload");
+    }
+
+    const newCompany = new Company({
+      companyName: req.body.companyName,
+      email: req.body.email,
+      password: req.body.password,
+      image: result ? result.secure_url : undefined,
+      cloudinaryId: result ? result.public_id : undefined,
+      companyDescription: req.body.companyDescription,
+    });
+
+    // Await the database query
+    const existingCompany = await Company.findOne({
+      $or: [{ email: req.body.email }, { companyName: req.body.companyName }],
+    });
+
+    if (existingCompany) {
+      req.flash("errors", {
+        msg: "Account with that email address or company name already exists.",
+      });
+      return res.redirect("../companySignup");
+    }
+
+    // Await the save operation
+    await newCompany.save();
+
+    req.logIn(newCompany, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/companyProfile");
+      console.log("company is logged in");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
